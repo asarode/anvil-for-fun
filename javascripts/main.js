3cef208d7c361408d3b8e346df4de9af485cbda3
Arjun Sarode <sarodearjun57@gmail.com>
2014-07-06 10:48:37 -0700

    Let's try again

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,21 +1,21 @@
-function initialize() {
+// function initialize() {
 
-	var mapCanvas = $("#menlo-park-map");
+// 	var mapCanvas = $("#menlo-park-map");
 
-	var mapOptions = {
-		center: new google.maps.LatLng(37.454055, -122.185123),
-		zoom: 8,
-		mapTypeId: google.maps.MapTypeId.ROADMAP
-	}
+// 	var mapOptions = {
+// 		center: new google.maps.LatLng(37.454055, -122.185123),
+// 		zoom: 8,
+// 		mapTypeId: google.maps.MapTypeId.ROADMAP
+// 	}
 
-	var map = new google.maps.Map(mapCanvas, mapOptions);
-}
+// 	var map = new google.maps.Map(mapCanvas, mapOptions);
+// }
 
 
 
-$(document).ready( function() {
-	initialize;
-	alert("done")
-});
+// $(document).ready( function() {
+// 	initialize;
+// 	alert("done")
+// });
 
 
