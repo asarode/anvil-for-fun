2c0ea9a47469bab72b400920347fa3a7661e8418
Arjun Sarode <sarodearjun57@gmail.com>
2014-07-07 09:36:05 -0700

    Add detail styles

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,21 +1,3 @@
-// function initialize() {
 
-// 	var mapCanvas = $("#menlo-park-map");
-
-// 	var mapOptions = {
-// 		center: new google.maps.LatLng(37.454055, -122.185123),
-// 		zoom: 8,
-// 		mapTypeId: google.maps.MapTypeId.ROADMAP
-// 	}
-
-// 	var map = new google.maps.Map(mapCanvas, mapOptions);
-// }
-
-
-
-// $(document).ready( function() {
-// 	initialize;
-// 	alert("done")
-// });
 
 
